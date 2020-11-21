516cf6c8497f6100a91e30401460cf390f318cb0
Oleg <ostotsky@gmail.com>
2020-11-21 15:34:15 +0300

    [stack] add doc

--- stack/stack.go
+++ stack/stack.go
@@ -1,3 +1,5 @@
+// Package stack provides immutable stack data structure.
+// All operations run on O(1) and are thread safe.
 package stack
 
 import "errors"
@@ -8,12 +10,20 @@ var (
 	emptyStackInstance   = emptyStack{}
 )
 
+// Stack ATD interface.
+//
+// Top returns the last element added to stack, or TopOfEmptyStackError if there is none, runs in O(1).
+//
+// Push adds a new element to the top of the stack and returns a new copy of stack, leaving the previous one untouched, runs in O(1).
+//
+// Pop returning a copy of the stack without the last element, or error if the method is called on empty stack, runs in O(1).
 type Stack interface {
 	Top() (interface{}, error)
 	Push(elem interface{}) Stack
 	Pop() (Stack, error)
 }
 
+// Creates an empty stack
 func NewStack() Stack {
 	return emptyStackInstance
 }
